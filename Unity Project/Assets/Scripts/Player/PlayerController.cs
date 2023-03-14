4e4279eb784349e3277ca883cc6e42e6d448e2e5
Feul617 <ho0papa617@gmail.com>
2023-03-15 00:16:26 +0900

    캐릭터 이동만 만듦
    번개스킬 옮겨야하는데 아직 프리팹에다가 뭘 넣어야 할지 감이 안 잡혀서 고민 좀 해보고 플레이어부터 어느 정도 완성해야 할 듯

--- Unity Project/Assets/Scripts/Player/PlayerController.cs
+++ Unity Project/Assets/Scripts/Player/PlayerController.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    Rigidbody rigid; //이거 이름 어캐해야할지 모르겠음
+    public LayerMask LayerMask;  //적만 감지, enemy == 6번
+
+    Vector3 moveVec;
+    public float speed;
+
+
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        Move();
+        Turn();
+    }
+
+    void Move()
+    {
+        moveVec = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;  //이동인데 따로 변수를 만드는게 좋을까?
+        transform.position += moveVec * speed * Time.deltaTime;
+    }
+
+    void Turn()
+    {
+        transform.LookAt(transform.position + moveVec);
+    }
+
+    void FindNearTarget()
+    {
+
     }
 }
