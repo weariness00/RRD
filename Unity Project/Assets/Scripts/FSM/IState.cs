1ee5eaa9025aedda0b83e9a2449062f4d81e918b
weariness00 <ehddn917@gmail.com>
2023-03-28 01:37:08 +0900

    IState 추가
    FSM 유한 상태 머신에 사용할 인터페이스

---
+++ Unity Project/Assets/Scripts/FSM/IState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IState
+{
+    void StateEnter<T>(T component) where T : UnityEngine.Component;
+    void StateUpdate();
+    void StateExit();
+}
