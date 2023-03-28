2ed7f337d9569b85da6b4a8d39feceb02ee380cc
weariness00 <ehddn917@gmail.com>
2023-03-28 20:13:20 +0900

    Util 수정
    list를 Swap 해주는 함수 추가

--- Unity Project/Assets/Scripts/System/Util/Util.cs
+++ Unity Project/Assets/Scripts/System/Util/Util.cs
@@ -88,6 +88,13 @@ public class Util
         
         return children;
     }
+
+    public static void Swap<T>(this List<T> list, int from, int to)
+    {
+        T tmp = list[from];
+        list[from] = list[to];
+        list[to] = tmp;
+    }
 }
 
 [System.Serializable]
