e51778618a3799b76776dc74ecec6adb710a9325
weariness00 <ehddn917@gmail.com>
2023-04-09 17:03:21 +0900

    기본 수정 사항

--- Unity Project/Assets/Scripts/Scene/LobbyScene.cs
+++ Unity Project/Assets/Scripts/Scene/LobbyScene.cs
@@ -1,26 +1,12 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class LobbyScene : BaseScene
 {
-    public GameObject charactorGrid;
-    List<Toggle> charactorSlots;
-
-    int selectCharactorIndex = -1;
     protected override void Init()
     {
         base.Init();
-        
-        foreach (Toggle toggle in Util.GetChildren<Toggle>(charactorGrid))
-        {
-            int temp = ++selectCharactorIndex;
-            toggle.onValueChanged.AddListener((bool isOn) => { if(isOn) CharactorIndex(temp); });
-
-            charactorSlots.Add(toggle);
-        }
     }
 
     public override void Clear()
@@ -30,10 +16,5 @@ public class LobbyScene : BaseScene
     public void GameStart()
     {
         Managers.Scene.LoadScene(SceneType.Game);
-    }
-
-    public void CharactorIndex(int index)
-    {
-        selectCharactorIndex = index;
     }
 }
