cff8ab35afda4641f61209548e527e19675065cf
weariness00 <ehddn917@gmail.com>
2023-05-08 21:05:27 +0900

    몬스터 dead 수정
    살아있는 개체수 감소하게 수정

--- Unity Project/Assets/Scripts/Game/Monster/Monster.cs
+++ Unity Project/Assets/Scripts/Game/Monster/Monster.cs
@@ -102,7 +102,7 @@ public class Monster : MonoBehaviour
         // 만약 필요하다면 파티클도
         // 아이템 루팅도 추가
         // 킬 카운트에 포함
-        //MonsterSpawnManager.Instance.aliveMonsterCount--;
+        MonsterSpawnManager.Instance.aliveMonsterCount--;
         // 다 끝난후 객체 소멸시키기
         Destroy(gameObject, 30f);
     }
