f1d6e69eb4da7dbe410bc9cfa6511d7dbe1bd0ed
weariness00 <ehddn917@gmail.com>
2023-05-31 02:13:08 +0900

    난이도의 시간 흐름 표시 완성
    시간흐름은 완성하였으나 시간이 흘러 다음 단계로 넘어갔을때 Monster을 강화하지 않는다.

--- Unity Project/Assets/Scripts/Game/Flow/Difficulty.cs
+++ Unity Project/Assets/Scripts/Game/Flow/Difficulty.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 [System.Serializable]
 public enum DifficultyType
@@ -25,14 +28,42 @@ public class Difficulty : MonoBehaviour
     public DifficultyData data;
     int index = 0;
 
+    public ScrollRect timeView;
+    float currentTime = 0;
+
+    Vector2 endPosition;
+
+    public void DifficultyUI_Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= data.NextDifficultyTime[index])
+            currentTime = data.NextDifficultyTime[index];
+
+        timeView.content.localPosition = 
+            Vector2.Lerp(
+                Vector2.zero,
+                endPosition,
+                currentTime / data.NextDifficultyTime[index]
+            );
+    }
     public void SetDifficulty(DifficultyType type)
     {
-        data = JsonUtil.LoadFile<DifficultyData>($"Difficulty/{type.ToString()}");
+        if(type == 0)
+            data = JsonUtil.LoadFile<DifficultyData>($"Difficulty/Easy");
+        else
+            data = JsonUtil.LoadFile<DifficultyData>($"Difficulty/{type.ToString()}");
+
+        StartCoroutine("ChangeDifficulty");
+        GameManager.Instance.UpdateCall.AddListener(DifficultyUI_Update);
     }
 
     IEnumerator ChangeDifficulty()
     {
-        yield return new WaitForSeconds(data.NextDifficultyTime[index++]);
-
+        foreach (float time in data.NextDifficultyTime)
+        {
+            endPosition = new Vector2(-(index + 1) * timeView.content.GetComponent<GridLayoutGroup>().cellSize.x, timeView.content.localPosition.y);
+            yield return new WaitForSeconds(time);
+            ++index;
+        }
     }
 }
--- Unity Project/Assets/Scripts/Game/Flow/DifficultyEditor.cs
+++ Unity Project/Assets/Scripts/Game/Flow/DifficultyEditor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [CanEditMultipleObjects]
 [CustomEditor(typeof(Difficulty))]
@@ -9,21 +10,24 @@ public class DifficultyEditor : Editor
 {
     SerializedProperty Path;
     SerializedProperty DifficultyData;
+    SerializedProperty TimeView;
 
     private void OnEnable()
     {
         Path = serializedObject.FindProperty("path");
         DifficultyData = serializedObject.FindProperty("data");
+        TimeView = serializedObject.FindProperty("timeView");
     }
 
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
         Difficulty difficulty = (Difficulty)target;
+        if (difficulty.data.type == 0)
+            EditorGUILayout.PropertyField(TimeView);
 
         EditorGUILayout.PropertyField(DifficultyData);
-        if(difficulty.data.type != 0)
-            CreatDifficultDataJson(difficulty);
+        if (difficulty.data.type != 0) CreatDifficultDataJson(difficulty);
 
         serializedObject.ApplyModifiedProperties();
     }
